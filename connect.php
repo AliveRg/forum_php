ee2e8748c64e93db148a0cc12399b51d203ebdc0
AliveRg <renatgorodov49280@gmail.com>
2023-12-24 16:18:18 +0300

    fix bag create table

--- connect.php
+++ connect.php
@@ -46,62 +46,72 @@
 
 $sql_create_table = "
 CREATE TABLE IF NOT EXISTS users (
-user_id INT AUTO_INCREMENT PRIMARY KEY,
-user_name VARCHAR(30) NOT NULL UNIQUE INDEX,
-user_pass VARCHAR(255) NOT NULL,
-user_email VARCHAR(255) NOT NULL,
-user_date DATETIME NOT NULL,
-user_level INT(8) NOT NULL,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(30) NOT NULL UNIQUE,
+    user_pass VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_date DATETIME NOT NULL,
+    user_level INT(8) NOT NULL
 );
 ";
 
 
 // Выполняем запрос
 if ($mysqli->query($sql_create_table) === TRUE) {
-   echo "Table created successfully";
+   echo "<br> Table created successfully";
 } else {
-   echo "Error creating table: " . $mysqli->error;
+   echo "Error creating table:  " . $mysqli->error;
 }
 
 
-// Таблица категорий
 $sql_create_table1 = "
-CREATE TABLE categories (
-cat_id INT(8) NOT NULL AUTO_INCREMENT,
-cat_name VARCHAR(255) NOT NULL,
-cat_description VARCHAR(255) NOT NULL, 
-UNIQUE INDEX cat_name_unique (cat_name), 
-PRIMARY KEY (cat_id)
+CREATE TABLE IF NOT EXISTS categories (
+    cat_id INT AUTO_INCREMENT PRIMARY KEY,
+    cat_name VARCHAR(255) NOT NULL UNIQUE,
+    cat_description VARCHAR(255) NOT NULL
 ) ENGINE=INNODB;
-
 ";
 
-// Таблица тем
+
+if ($mysqli->query($sql_create_table1) === TRUE) {
+   echo "<br> Table created successfully";
+} else {
+   echo "Error creating table: " . $mysqli->error;
+};
+
 $sql_create_table2 = "
-CREATE TABLE topics (
-   topic_id INT(8) NOT NULL AUTO_INCREMENT,
-   topic_subject VARCHAR(255) NOT NULL,
-   topic_date DATETIME NOT NULL,
-   topic_cat INT(8) NOT NULL,
-   topic_by INT(8) NOT NULL, 
-   PRIMARY KEY (topic_id) 
-   ) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS topics (
+    topic_id INT AUTO_INCREMENT PRIMARY KEY,
+    topic_subject VARCHAR(255) NOT NULL,
+    topic_date DATETIME NOT NULL,
+    topic_cat INT NOT NULL,
+    topic_by INT NOT NULL,
+    FOREIGN KEY (topic_cat) REFERENCES categories(cat_id),
+    FOREIGN KEY (topic_by) REFERENCES users(user_id)
+) ENGINE=INNODB;
 ";
+
+if ($mysqli->query($sql_create_table2) === TRUE) {
+   echo "<br> Table created successfully";
+} else {
+   echo "Error creating table: " . $mysqli->error;
+};
 
 // Таблица сообщений
 $sql_create_table3 = "
-CREATE TABLE posts (
-   post_id INT(8) NOT NULL AUTO_INCREMENT,
-   post_content TEXT NOT NULL,
-   post_date DATETIME NOT NULL,
-   post_topic INT(8) NOT NULL,
-   post_by INT(8) NOT NULL, 
-   PRIMARY KEY (post_id) 
-   ) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS posts (
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    post_content TEXT NOT NULL,
+    post_date DATETIME NOT NULL,
+    post_topic INT NOT NULL,
+    post_by INT NOT NULL,
+    FOREIGN KEY (post_topic) REFERENCES topics(topic_id),
+    FOREIGN KEY (post_by) REFERENCES users(user_id)
+) ENGINE=INNODB;
 ";
 
-if ($mysqli->query($sql_create_table) === TRUE) {
-   echo "Table created successfully";
+if ($mysqli->query($sql_create_table3) === TRUE) {
+   echo "<br> Table created successfully";
 } else {
    echo "Error creating table: " . $mysqli->error;
 };
