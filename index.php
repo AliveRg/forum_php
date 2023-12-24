609ccda0d6ced1413a4ce391c81e0d2f3b980beb
AliveRg <renatgorodov49280@gmail.com>
2023-12-24 17:53:50 +0300

    add viewer cat

--- index.php
+++ index.php
@@ -5,14 +5,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .container {
+            border: solid 2px #000;
+            width: 500px;
+            padding: 10px 40px;
+            margin: 50px auto;
+            display: flex;
+            flex-direction: column;
+            align-items: start;
+            justify-content: space-between;
 
+        }
+
+        .field {
+            height: 10px;
+        }
+    </style>
 </head>
 
 <?php
     include './connect.php';
     include './header.php';
 
+    // SQL-запрос для выборки всех данных из таблицы
+    $sql_select_data = "SELECT * FROM categories";
+    $result = $mysqli->query($sql_select_data);
     
+    if ($result->num_rows > 0) {
+        // Выводим данные
+        while($row = mysqli_fetch_assoc($result)) {
+          echo "<div class='container'>";
+          
+         
+          echo "<h2 class='field'> " . $row["cat_name"] . "</h2>";
+          echo "<h4 class='field'> " . $row["cat_description"] . "</h4>";
+        echo "</div>";
+          echo "<hr>";
+      }
+      
+    
+    
+       
+        
+    } else {
+        echo "0 results";
+    }
+
 
     include './footer.php';
 ?>
