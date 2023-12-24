0d935c82be000ffa555853765afd23804ac93a2a
AliveRg <renatgorodov49280@gmail.com>
2023-12-24 23:58:50 +0300

    session isset

--- connect.php
+++ connect.php
@@ -2,13 +2,13 @@
 <html lang="en">
 
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
-   <?php
+    <?php
 
    
    $db_host = 'localhost';
@@ -23,7 +23,10 @@
      $db_db
    );
 
-   session_start();
+   if(!isset($_SESSION)) 
+   { 
+       session_start(); 
+   } 
     if (!isset($_SESSION['signed_in'])) {
         $_SESSION['signed_in'] = false;
     }
